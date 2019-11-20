89eb3bc33ca1bb522a0a0b5aa4d68be65515afc8
Macarena <macarenapomerantz@gmail.com>
2019-11-20 03:49:42 -0300

    Nuevas Paginas de Busqueda e Informacion

---
+++ boxme-front/src/pages/SearchPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Menu from '../components/Navbar/Menu'
+import BodySearch from '../components/BodySearch/BodySearch';
+
+function SearchPage() {
+    return (
+      <div>
+        <Menu/>
+        <BodySearch/>
+      </div>   
+    );
+  }
+  
+  export default SearchPage;
